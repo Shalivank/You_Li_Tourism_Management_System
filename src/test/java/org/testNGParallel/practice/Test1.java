5e0af3f392ee2aedcf6e2d81d2f02f5ec99998a2
SHALIVAN KHANDRE <SHALIVAN KHANDRE@SHALIVAN-KHANDRE>
2022-09-21 13:01:29 +0530

    changes done by engg-1 in Test! in You-Li Branch

--- src/test/java/org/testNGParallel/practice/Test1.java
+++ src/test/java/org/testNGParallel/practice/Test1.java
@@ -12,7 +12,7 @@ public class Test1 {
    // changes done by engg-1 second time
 
    //changes done by engg-2
-   //changes done by engg-1 in You-Li_01
+ //changes done by engg-1 in You-Li_01
    @Test
    public void test2()
    {
